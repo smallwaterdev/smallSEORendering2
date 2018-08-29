3719e0a47666eeb14c47c961d1c1ff96fd82315c
Nate SmallWater <smallwaterdev@gmail.com>
2018-08-29 17:16:47 -0400

    release

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-const smallDataUserAddr = "http://www.javferry.com/user/api";
-
+//const smallDataUserAddr = "http://www.javferry.com/user/api";
+const smallDataUserAddr = "http://localhost:3000/user/api";
 module.exports.smallDataUserAddr = smallDataUserAddr;
